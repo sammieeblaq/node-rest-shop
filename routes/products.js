cf78f88f6a26fe198642f69ab20b732ee841da78
Samuel Airehrour <odianoseairehrour@gmail.com>
2019-11-28 02:09:44 +0100

    The file upload object has been abstacted to an external file

--- routes/products.js
+++ routes/products.js
@@ -1,34 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
 const auth = require("../middleware/auth");
+const upload = require("../middleware/fileUpload");
 
 const productController = require("../controllers/products");
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
-    }
-})
 
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-}
-
-const upload = multer({ 
-    storage: storage, 
-    limits: {
-        fileSize: 1024 * 1024 * 5,
-    },
-    fileFilter: fileFilter
-})
 
 // Route Handlers
 router.get("/", productController.getProducts);
